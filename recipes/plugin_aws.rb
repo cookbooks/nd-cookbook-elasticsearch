8a02154fcf59df8b957599991046c77d329549cb
Cameron Johnston <cameron@needle.com>
2012-06-29 15:35:35 -0600

    fixing incorrect use of secrets in plugin_aws.rb

--- recipes/plugin_aws.rb
+++ recipes/plugin_aws.rb
@@ -5,8 +5,8 @@ secrets = Chef::EncryptedDataBagItem.load("secrets", node.chef_environment, data
 
 if node.has_key?('ec2')
 	aws_resource_tag node['ec2']['instance_id'] do
-		aws_access_key aws['aws_access_key_id']
-		aws_secret_access_key aws['aws_secret_access_key']
+		aws_access_key secrets['aws']['aws_access_key_id']
+		aws_secret_access_key secrets['aws']['aws_secret_access_key']
 		tags({"Name" => "ElasticSearch (#{node.chef_environment})",
 			"Environment" => node.chef_environment})
 		action :update

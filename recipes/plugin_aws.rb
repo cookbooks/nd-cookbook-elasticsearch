63c18496c4490b34a29f08afe65590383d183dc8
Cameron Johnston <cameron@needle.com>
2012-07-06 10:52:50 -0600

    adding logic to generate cluster-specific ec2 tags, fixing incorrect variable names for aws credentials

--- recipes/plugin_aws.rb
+++ recipes/plugin_aws.rb
@@ -3,11 +3,18 @@ include_recipe 'aws'
 data_bag_key = Chef::EncryptedDataBagItem.load_secret(node['data_bag_key'])
 secrets = Chef::EncryptedDataBagItem.load("secrets", node.chef_environment, data_bag_key)
 
+case node.elasticsearch[:cluster_name]
+when "elasticsearch"
+	nametag = "ElasticSearch (#{node.chef_environment})"
+else
+	nametag = "ElasticSearch (#{node.chef_environment}): #{node.elasticsearch[:cluster_name]}"
+end
+
 if node.has_key?('ec2')
 	aws_resource_tag node['ec2']['instance_id'] do
-		aws_access_key secrets['aws']['aws_access_key_id']
-		aws_secret_access_key secrets['aws']['aws_secret_access_key']
-		tags({"Name" => "ElasticSearch (#{node.chef_environment})",
+		aws_access_key secrets['aws']['elasticsearch']['aws_access_key_id']
+		aws_secret_access_key secrets['aws']['elasticsearch']['aws_secret_access_key']
+		tags({"Name" => nametag,
 			"Environment" => node.chef_environment})
 		action :update
 	end
